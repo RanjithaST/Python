101ffc3f5ffbc926a01393b41291102ca0b47816
Ranjitha S T <ranjithast009@gmail.com>
2026-02-09 17:40:03 +0530

    Employee class added

--- Employee.py
+++ Employee.py
@@ -1,16 +1,16 @@
 class Employee:
     def __init__(self):
         self.empName=None
-        self.empRoll=None
+        self.empID=None
 emp=Employee()
 print(emp)
 print("**********BEFORE DATA ASSIGN**************")
 print(emp.empName)
-print(emp.empRoll)
+print(emp.empID)
 emp.empName="John"
 emp.empRoll=101
 print("**********AFTER DATA ASSIGN**************")
 print(emp.empName)
-print(emp.empRoll)
+print(emp.empID)
 emp1=Employee()
 print(emp1)
