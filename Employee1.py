7240a8ea4b6909225870d1182f103a97652335d6
Ranjitha S T <ranjithast009@gmail.com>
2026-02-11 20:51:46 +0530

    Local variable

---
+++ Employee1.py
@@ -0,0 +1,20 @@
+class Employee:
+    companyName="DCL"
+    def __init__(self):
+        self.empID=None
+        self.empName=None #1st way to create instance variable
+        print(self)
+e1=Employee()
+print(e1)
+print("***Invoked from class object***",Employee.companyName)
+print("***Invoked from class object***",e1.companyName)
+e1.companyName="Google" ##2nd way to create instance variable
+print("***Invoked from object***",e1.companyName)
+e1.salary=1000
+print(e1.salary)
+
+#Reading/accessing the class variable can be accessed by the class variable as well as object reference
+#modifing/writing the class variable only by class name 
+#while reading using object reference PVM searches in object dict if not found it searches in class dict and fake it and print
+#while modifing using object reference PVM searches in object dict if not found it creates inside the object without modifying class variable
+#while modifing using class variable it directly modifies the class variable 
---
+++ Employee2.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self):
+        salary=1000
+        print("Inside init",salary)
+        print("***END***")
+    print("Outside INIT not allowed")
+print("Outside class not allowed")
+e1=Employee()#need to be created
+e1=Employee()#need to be created
+print("Outside INIT not allowed")
+print("Outside class not allowed")
